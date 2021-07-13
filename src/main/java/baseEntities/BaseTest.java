83330b771fb4b198e9022083c3a4d631783ba03d
Olga Shevchuk <morozolga92@gmail.com>
2021-07-13 23:49:12 +0300

    add BrowserService

--- src/main/java/baseEntities/BaseTest.java
+++ src/main/java/baseEntities/BaseTest.java
@@ -1,10 +1,8 @@
 package baseEntities;
 
+import core.BrowserService;
 import core.ReadProperties;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -17,14 +15,13 @@ public class BaseTest {
     @BeforeTest
     public void setupTest(){
 
-        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         properties = new ReadProperties();
     }
 
     @BeforeMethod
     public void setupMethod(){
 
-        driver = new ChromeDriver();
+        driver = new BrowserService().getDriver();
     }
 
     @AfterMethod
