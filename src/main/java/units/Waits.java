b900c633920510add105e395206e997af3d88ed2
Olga Shevchuk <morozolga92@gmail.com>
2021-07-26 00:25:33 +0300

    add elements Button and Checkbox

---
+++ src/main/java/units/Waits.java
@@ -0,0 +1,13 @@
+package units;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Waits {
+    public Waits(WebDriver driver) {
+
+    }
+
+    public void waitForClickable(WebElement webElement) {
+    }
+}
